6217532282b9c33bd9a6cf9e1a4094b7eb20c4f6
Bruno <brunodias40@gmail.com>
2025-03-10 21:15:26 -0300

    fix: update react-phone-number-input version and add CSS import

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -14,6 +14,7 @@ import { Control } from "react-hook-form";
 import { Textarea } from "./ui/textarea";
 import { Checkbox } from "./ui/checkbox";
 import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
 import { E164Number } from "libphonenumber-js/core";
 import { SelectContent, SelectValue, SelectTrigger, Select } from "./ui/select";
 import ReactDatePicker from "react-datepicker";
